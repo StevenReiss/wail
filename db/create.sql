f4e7c57b0b71401599db69586b5a83fe31cd4147
Steven Reiss <spr@cs.brown.edu>
2020-03-24 10:06:53 -0400

    Add redesign lesson and its crits.

--- db/create.sql
+++ db/create.sql
@@ -60,6 +60,7 @@ INSERT INTO lessons (name,id,module,reference,description)
 VALUES
    ("Design 1", "design1", "designlesson", NULL, "Quick Web Page Design Example" ),
    ("Crits 1", "crits1", "critslesson", "design1", "Crits on Quick Web Page Design Example" ),
+   ("Design 2", "design2", "designlesson", NULL, "Quick Web Page Redesign Example" ),
    ("SQL Injection", "sqlinjection", "sqlinjectionlesson", NULL, "SQL Injection Attacks" ),
    ("XSS Attack", "xss", "xsslesson", NULL, "Cross-Site Scripting Attacks" ),
    ("Define User Test", "usertest1", "usertestlesson", NULL, "Define a user test for your final project" ),
